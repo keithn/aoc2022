297da51504b7ebbf00524ace0cb299f85ad950e9
Keith Nicholas <keith.nicholas@gmail.com>
2022-12-09 19:08:02 +1300

    Day 9 done

--- days/Day9.cs
+++ days/Day9.cs
@@ -2,7 +2,6 @@
 
 public class Day9
 {
-
     public record struct Point(int X, int Y)
     {
         public Point Move((int dx, int dy) direction) => new(X + direction.dx, Y + direction.dy);
@@ -14,40 +13,39 @@ public class Day9
             return new Point(X + int.Clamp(x,-1,1), Y + int.Clamp(y,-1,1));
         }
     }
-
     public static void Solve()
     {
         var lines = File.ReadLines("days/Day9.txt");
-        var trail = TailTrail(lines, Enumerable.Range(1, 2).Select(_ => new Point(0, 0)).ToList());
-        Console.WriteLine($"Part 1: {trail.Distinct().Count()}");
-        trail = TailTrail(lines, Enumerable.Range(1, 10).Select(_ => new Point(0, 0)).ToList());
-        Console.WriteLine($"Part 2: {trail.Distinct().Count()}");
-    }
 
-    private static List<Point> TailTrail(IEnumerable<string> lines, List<Point> rope)
-    {
-        var trail = new List<Point>();
-        foreach (var line in lines)
+        IEnumerable<Point> TailTrail(IEnumerable<string> lines, List<Point> rope)
         {
-            var movement = line.Split();
-            var direction = movement[0] switch
+            var tailTrail = new List<Point>();
+            foreach (var line in lines)
             {
-                "R" => (1, 0),
-                "L" => (-1, 0),
-                "U" => (0, 1),
-                "D" => (0, -1),
-            };
-            var count = int.Parse(movement[1]);
-            for (int _ = 0; _ < count; _++)
-            {
-                rope[0] = rope[0].Move(direction);
-                for(var k = 1; k<rope.Count;k++)
+                var movement = line.Split();
+                var direction = movement[0] switch
                 {
-                    rope[k] = rope[k].Follow(rope[k-1]);
-                    if(k == rope.Count-1) trail.Add(rope[k]);
+                    "R" => (1, 0),
+                    "L" => (-1, 0),
+                    "U" => (0, 1),
+                    "D" => (0, -1),
+                };
+                var count = int.Parse(movement[1]);
+                for (int _ = 0; _ < count; _++)
+                {
+                    rope[0] = rope[0].Move(direction);
+                    for (var k = 1; k < rope.Count; k++)
+                    {
+                        rope[k] = rope[k].Follow(rope[k - 1]);
+                        if (k == rope.Count - 1) tailTrail.Add(rope[k]);
+                    }
                 }
             }
+            return tailTrail.Distinct();
         }
-        return trail;
+        var trail = TailTrail(lines, Enumerable.Range(1, 2).Select(_ => new Point(0, 0)).ToList());
+        Console.WriteLine($"Part 1: {trail.Count()}");
+        trail = TailTrail(lines, Enumerable.Range(1, 10).Select(_ => new Point(0, 0)).ToList());
+        Console.WriteLine($"Part 2: {trail.Count()}");
     }
 }
