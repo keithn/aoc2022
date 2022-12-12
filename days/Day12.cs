adfd4cb7213c0f394e792965c5774f1700977719
Keith Nicholas <keith.nicholas@gmail.com>
2022-12-13 01:05:00 +1300

    Day12 Rendering both parts

--- days/Day12.cs
+++ days/Day12.cs
@@ -41,6 +41,7 @@ public class Day12
         {
             var start = terrain.SelectMany((l, y) => l.Select((h, x) => new WayPoint(h.Height == beginAt ? x : -1, y))).First(p => p.X > -1);
             Queue<WayPoint> queue = new(new []{start});
+            int i = 0;
             while (queue.Any())
             {
                 var p = queue.Dequeue();
@@ -51,6 +52,7 @@ public class Day12
                     queue.Enqueue(n);
                 });
                 terrain[p.Y][p.X].Visited = true;
+                i++;
             }
             return null; // no way!
         }
@@ -61,10 +63,11 @@ public class Day12
         var path2 = Navigate(ToTerrain(lines), 'E', 'a', (f, t) => f - t <= 1);
         Console.WriteLine($"Part 2: {path2.Seen().Count() }");
         
-        Render(lines, path);
+        Render(lines.ToList(), path, "day12-part1.png");
+        Render(lines.ToList(), path2, "day12-part2.png");
     }
 
-    public static void Render(List<string> lines, WayPoint wayPoint)
+    private static void Render(List<string> lines, WayPoint wayPoint, string filename = "Day12.png")
     {
         foreach (var point in wayPoint.Seen())
         {
@@ -72,6 +75,39 @@ public class Day12
             line[point.X] = 'P';
             lines[point.Y] = new string(line);
         }
-        TextToImage.From(lines, "day12.png");
+        TextToImage.From(lines, filename, TerrainColorMap);
     }
+    private static readonly Dictionary<char?, string> TerrainColorMap = new()
+    {
+        { '#', "4E892A" },
+        { 'a', "fafa6e" },
+        { 'b', "e6f56f" },
+        { 'c', "d3f072" },
+        { 'd', "c0eb75" },
+        { 'e', "ade578" },
+        { 'f', "9cdf7c" },
+        { 'g', "8ad97f" },
+        { 'h', "7ad283" },
+        { 'i', "69cb86" },
+        { 'j', "5ac489" },
+        { 'k', "4abd8c" },
+        { 'l', "3bb58e" },
+        { 'm', "2bad8f" },
+        { 'n', "1aa68f" },
+        { 'o', "059e8f" },
+        { 'p', "00968e" },
+        { 'q', "008e8c" },
+        { 'r', "008689" },
+        { 's', "007e86" },
+        { 't', "027681" },
+        { 'u', "106e7c" },
+        { 'v', "196676" },
+        { 'w', "1f5e6f" },
+        { 'x', "245768" },
+        { 'y', "284f60" },
+        { 'z', "2a4858" },
+        { 'S', "FF0000" },
+        { 'P', "AAAA0000" },
+        { 'E', "AAAA0000" },
+    };
 }
